fcbb29a816e9c81a94bbe11ed7ebd1de8f16c2ba
David Henley <henleydr@gmail.com>
2017-10-13 10:20:55 -0500

    WebSocket connection

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
 
 app.use(express.static('public'));
 
-app.listen(4000, () => console.log('Listening on 4000'));
+const server = app.listen(4000, () => console.log('Listening on 4000'));
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('WebSocket is connected');
+});
